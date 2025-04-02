2aa7d84fb9375415eecae05f049ec54af49ff934
Luxus <dera.ah.14@gmail.com>
2025-04-02 21:08:18 +0200

    quill works

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:e_contrat/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const Econtrat());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
